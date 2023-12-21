576a75f7ad8f73ee2a4d9fdf3a5e39fe871e0a91
pascalcortez <pascal@musicredone.com>
2023-12-21 00:44:21 -0500

    Ok Version

--- functions/scan.js
+++ functions/scan.js
@@ -2,73 +2,79 @@ const functions = require("firebase-functions");
 const fetch = require("node-fetch");
 const cors = require("cors")({ origin: true });
 
-const scan = functions.https.onRequest((request, response) => {
-  cors(request, response, async () => {
-    const shopifyDomain = "musique-red-one-music.myshopify.com";
-    const adminApiAccessToken = "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";
-    const upc = request.query.upc;
+const runtimeOpts = {
+  minInstances: 5,
+};
 
-    console.log("Received UPC:", upc);
+const scan = functions
+  .runWith(runtimeOpts)
+  .https.onRequest((request, response) => {
+    cors(request, response, async () => {
+      const shopifyDomain = "musique-red-one-music.myshopify.com";
+      const adminApiAccessToken = "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";
+      const upc = request.query.upc;
 
-    if (!upc) {
-      response.status(400).send("Missing UPC parameter");
-      return;
-    }
+      console.log("Received UPC:", upc);
 
-    const graphqlQuery = JSON.stringify({
-      query: `query MyQuery {
+      if (!upc) {
+        response.status(400).send("Missing UPC parameter");
+        return;
+      }
+
+      const graphqlQuery = JSON.stringify({
+        query: `query MyQuery {
         products(first: 1, query: "barcode:${upc}") {
           nodes {
             id
           }
         }
       }`,
-    });
+      });
 
-    console.log("GraphQL Query:", graphqlQuery);
+      console.log("GraphQL Query:", graphqlQuery);
 
-    try {
-      const shopifyAdminResponse = await fetch(
-        `https://${shopifyDomain}/admin/api/2023-10/graphql.json`,
-        {
-          method: "POST",
-          headers: {
-            "X-Shopify-Access-Token": adminApiAccessToken,
-            "Content-Type": "application/json",
-          },
-          body: graphqlQuery,
+      try {
+        const shopifyAdminResponse = await fetch(
+          `https://${shopifyDomain}/admin/api/2023-10/graphql.json`,
+          {
+            method: "POST",
+            headers: {
+              "X-Shopify-Access-Token": adminApiAccessToken,
+              "Content-Type": "application/json",
+            },
+            body: graphqlQuery,
+          }
+        );
+
+        const jsonResponse = await shopifyAdminResponse.json();
+        console.log("Shopify Response:", jsonResponse);
+
+        if (jsonResponse.errors) {
+          console.error("Shopify API Error:", jsonResponse.errors);
+          response.status(500).send("Shopify API Error");
+          return;
         }
-      );
 
-      const jsonResponse = await shopifyAdminResponse.json();
-      console.log("Shopify Response:", jsonResponse);
+        if (!jsonResponse.data || !jsonResponse.data.products) {
+          console.error("Invalid response structure:", jsonResponse);
+          response.status(500).send("Invalid response structure");
+          return;
+        }
 
-      if (jsonResponse.errors) {
-        console.error("Shopify API Error:", jsonResponse.errors);
-        response.status(500).send("Shopify API Error");
-        return;
+        const nodes = jsonResponse.data.products.nodes;
+        if (nodes.length > 0) {
+          const productId = nodes[0].id;
+          console.log("Product ID:", productId);
+          response.send({ productId });
+        } else {
+          console.log("No product found for UPC:", upc);
+          response.status(404).send("Product not found");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        response.status(500).send("Server error");
       }
-
-      if (!jsonResponse.data || !jsonResponse.data.products) {
-        console.error("Invalid response structure:", jsonResponse);
-        response.status(500).send("Invalid response structure");
-        return;
-      }
-
-      const nodes = jsonResponse.data.products.nodes;
-      if (nodes.length > 0) {
-        const productId = nodes[0].id;
-        console.log("Product ID:", productId);
-        response.send({ productId });
-      } else {
-        console.log("No product found for UPC:", upc);
-        response.status(404).send("Product not found");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      response.status(500).send("Server error");
-    }
+    });
   });
-});
 
 module.exports = { scan };
--- functions/search.js
+++ functions/search.js
@@ -2,14 +2,20 @@ const functions = require("firebase-functions");
 const fetch = require("node-fetch");
 const cors = require("cors")({ origin: true });
 
-const search = functions.https.onRequest((request, response) => {
-  cors(request, response, async () => {
-    const shopifyDomain = "musique-red-one-music.myshopify.com";
-    const storefrontAccessToken = "ff22e43cfa2c734aea496f1307b9370b";
-    const searchQuery = request.query.q;
+const runtimeOpts = {
+  minInstances: 5,
+};
 
-    const graphqlQuery = JSON.stringify({
-      query: `
+const search = functions
+  .runWith(runtimeOpts)
+  .https.onRequest((request, response) => {
+    cors(request, response, async () => {
+      const shopifyDomain = "musique-red-one-music.myshopify.com";
+      const storefrontAccessToken = "ff22e43cfa2c734aea496f1307b9370b";
+      const searchQuery = request.query.q;
+
+      const graphqlQuery = JSON.stringify({
+        query: `
         query MyQuery($searchQuery: String!) {
           products(first: 18, query: $searchQuery) {
             nodes {
@@ -44,31 +50,31 @@ const search = functions.https.onRequest((request, response) => {
           }
         }
       `,
-      variables: {
-        searchQuery: searchQuery,
-      },
+        variables: {
+          searchQuery: searchQuery,
+        },
+      });
+
+      try {
+        const shopifyResponse = await fetch(
+          `https://${shopifyDomain}/api/2023-10/graphql.json`,
+          {
+            method: "POST",
+            headers: {
+              "X-Shopify-Storefront-Access-Token": storefrontAccessToken,
+              "Content-Type": "application/json",
+            },
+            body: graphqlQuery,
+          }
+        );
+
+        const jsonResponse = await shopifyResponse.json();
+        response.send(jsonResponse.data.products.nodes);
+      } catch (error) {
+        console.error("Error:", error);
+        response.status(500).send("Server error");
+      }
     });
-
-    try {
-      const shopifyResponse = await fetch(
-        `https://${shopifyDomain}/api/2023-10/graphql.json`,
-        {
-          method: "POST",
-          headers: {
-            "X-Shopify-Storefront-Access-Token": storefrontAccessToken,
-            "Content-Type": "application/json",
-          },
-          body: graphqlQuery,
-        }
-      );
-
-      const jsonResponse = await shopifyResponse.json();
-      response.send(jsonResponse.data.products.nodes);
-    } catch (error) {
-      console.error("Error:", error);
-      response.status(500).send("Server error");
-    }
   });
-});
 
 module.exports = { search };
